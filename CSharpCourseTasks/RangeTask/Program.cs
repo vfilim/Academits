6512d73bec114ccbb98c0bbd6d21ae64ab7d91fd
Vlad <rashovlad@gmail.com>
2020-07-07 14:51:50 +0700

    Сделал первую часть задачи range

---
+++ CSharpCourseTasks/RangeTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range(2, 5.7);
+            double number = 3;
+
+            Console.WriteLine(range.GetLength());
+            Console.WriteLine(range.IsInside(number));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpCourseTasks/RangeTask/Range.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From
+        {
+            get;
+            set;
+        }
+
+        public double To
+        {
+            get;
+            set;
+        }
+
+        public Range(double from, double to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            if ((number >= From) && (number <= To))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
