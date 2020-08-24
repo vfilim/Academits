9c795bdb01bfcdb6c2042f69999f610d28138e75
Vlad <rashovlad@gmail.com>
2020-08-24 23:06:59 +0700

    Сделал Matrix без реализации определителя

--- CSharpCourseTasks/MatrixTask/Matrix.cs
+++ CSharpCourseTasks/MatrixTask/Matrix.cs
@@ -1,123 +1,242 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using VectorTask;
 
 namespace MatrixTask
 {
     public class Matrix
     {
-        public Vector[] Vectors { get; set; }
+        private Vector[] strings;
 
-        public Matrix(int m, int n)
+        public Matrix(int stringsCount, int columnsCount)
         {
-            Vectors = new Vector[m];
-
-            for (int i = 0; i < m; i++)
+            if (stringsCount < 1)
             {
-                Vectors[i] = new Vector(n);
+                throw new ArgumentException("Strings count can't be < 1, it is " + stringsCount, nameof(stringsCount));
+            }
+
+            if (stringsCount < 1)
+            {
+                throw new ArgumentException("Columns count can't be < 1, it is " + columnsCount, nameof(columnsCount));
+            }
+
+            strings = new Vector[stringsCount];
+
+            for (int i = 0; i < stringsCount; i++)
+            {
+                strings[i] = new Vector(columnsCount);
             }
         }
 
         public Matrix(Matrix matrix)
         {
-            Vectors = new Vector[matrix.Vectors.Length];
+            strings = new Vector[matrix.strings.Length];
 
-            for (int i = 0; i < matrix.Vectors.Length; i++)
+            for (int i = 0; i < matrix.strings.Length; i++)
             {
-                Vectors[i] = new Vector(matrix.Vectors[i]);
+                strings[i] = new Vector(matrix.strings[i]);
             }
         }
 
         public Matrix(double[,] numbers)
         {
-            Vectors = new Vector[numbers.GetLength(0)];
+            if (numbers.Length < 1)
+            {
+                throw new ArgumentException("Elements count can't be 0", nameof(numbers));
+            }
+
+            strings = new Vector[numbers.GetLength(0)];
 
             for (int i = 0; i < numbers.GetLength(0); i++)
             {
-                Vectors[i] = new Vector(numbers.GetLength(1));
+                strings[i] = new Vector(numbers.GetLength(1));
             }
 
             for (int i = 0; i < numbers.GetLength(0); i++)
             {
                 for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    Vectors[i].Coordinates[j] = numbers[i, j];
+                    strings[i].SetCoordinate(j, numbers[i, j]);
                 }
             }
         }
 
-        public Matrix(Vector[] vectors)
+        public Matrix(params Vector[] strings)
         {
-            Vectors = vectors;
-        }
-
-        public int[] GetSize()
-        {
-            int[] size = new int[2];
-
-            size[0] = Vectors.Length;
-            size[1] = Vectors[0].GetSize();
-
-            return size;
-        }
-
-        public Vector GetColumn(int j)
-        {
-            Vector column = new Vector(Vectors.Length);
-
-            for (int i = 0; i < Vectors.Length; i++)
+            if (strings.Length == 0)
             {
-                column.Coordinates[i] = Vectors[i].Coordinates[j];
+                throw new ArgumentException("Strings count can't be 0", nameof(strings));
+            }
+
+            int coordinatesCount = 0;
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].GetSize() > coordinatesCount)
+                {
+                    coordinatesCount = strings[i].GetSize();
+                }
+            }
+
+            Vector[] matrixStrings = new Vector[strings.Length];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                matrixStrings[i] = new Vector(coordinatesCount);
+
+                for (int j = 0; j < strings[i].GetSize(); j++)
+                {
+                    matrixStrings[i].SetCoordinate(j, strings[i].GetCoordinate(j));
+                }
+            }
+
+            this.strings = matrixStrings;
+        }
+
+        public int GetStringsCount()
+        {
+            return strings.Length;
+        }
+
+        public int GetColumnsCount()
+        {
+            return strings[0].GetSize();
+        }
+
+        public Vector GetColumn(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("The index can't be < 0 (it is " + index + ")", nameof(index));
+            }
+
+            Vector column = new Vector(strings.Length);
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                column.SetCoordinate(i, strings[i].GetCoordinate(index));
             }
 
             return column;
         }
 
-        public void GetTransposed()
+        public void Transpose()
         {
-            Matrix transposed = new Matrix(GetSize()[1], GetSize()[0]);
+            Matrix transposed = new Matrix(GetColumnsCount(), GetStringsCount());
 
-            for (int i = 0; i < GetSize()[1]; i++)
+            for (int i = 0; i < GetColumnsCount(); i++)
             {
-                transposed.Vectors[i] = GetColumn(i);
+                transposed.strings[i] = GetColumn(i);
             }
 
-            Vectors = transposed.Vectors;
+            strings = transposed.strings;
         }
 
         public void MultiplyOnNumber(double number)
         {
-            for (int i = 0; i < Vectors.Length; i++)
+            for (int i = 0; i < strings.Length; i++)
             {
-                for (int j = 0; j < Vectors[0].GetSize(); j++)
-                {
-                    Vectors[i].Coordinates[j] *= number;
-                }
+                strings[i].MultiplyOnNumber(number);
             }
         }
 
         public void MultiplyOnVector(Vector vector)
         {
-            
+            int vectorSize = vector.GetSize();
+
+            if (vectorSize != GetColumnsCount())
+            {
+                throw new ArgumentException("The count of matrix columns must be equal to the vector coordinates count, they are " + GetStringsCount() + " and " + vector.GetSize(), nameof(vector));
+            }
+
+            double[] coordinates = new double[vectorSize];
+
+            for (int i = 0; i < vectorSize; i++)
+            {
+                for (int j = 0; j < vectorSize; j++)
+                {
+                    coordinates[i] += strings[i].GetCoordinate(j) * vector.GetCoordinate(j);
+                }
+            }
+
+            Array.Resize<Vector>(ref strings, 1);
+
+            Vector resultVector = new Vector(coordinates);
+
+            strings[0] = resultVector;
+        }
+
+        public double GetDeterminant()
+        {
+            double determinant = 0;
+
+
+
+            return determinant;
         }
 
         public void AddMatrix(Matrix matrix)
         {
-            for (int i = 0; i < Vectors.Length; i++)
+            if (GetStringsCount() != matrix.GetStringsCount() || GetColumnsCount() != matrix.GetColumnsCount())
             {
-                Vectors[i].Add(matrix.Vectors[i]);
+                throw new ArgumentException("The matrices sizes must be the same. They are (" + GetStringsCount() + ", " + GetColumnsCount() + ") and (" + matrix.GetStringsCount() + ", " + GetColumnsCount(), nameof(matrix));
+            }
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i].Add(matrix.strings[i]);
             }
         }
 
         public void SubtractMatrix(Matrix matrix)
         {
-            for (int i = 0; i < Vectors.Length; i++)
+            if (GetStringsCount() != matrix.GetStringsCount() || GetColumnsCount() != matrix.GetColumnsCount())
             {
-                Vectors[i].Subtract(matrix.Vectors[i]);
+                throw new ArgumentException("The matrices sizes must be the same. They are (" + GetStringsCount() + ", " + GetColumnsCount() + ") and (" + matrix.GetStringsCount() + ", " + GetColumnsCount(), nameof(matrix));
             }
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i].Subtract(matrix.strings[i]);
+            }
+        }
+
+        public static Matrix GetMatrixSum(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix sum = new Matrix(matrix1);
+
+            sum.AddMatrix(matrix2);
+
+            return sum;
+        }
+
+        public static Matrix GetMatrixDifference(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix dif = new Matrix(matrix1);
+
+            dif.SubtractMatrix(matrix2);
+
+            return dif;
+        }
+
+        public static Matrix GetMatrixMultiplication(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.GetColumnsCount() != matrix2.GetStringsCount())
+            {
+                throw new ArgumentException("The first matrix columns count must be equal to the second matrix strings count, they are " + matrix1.GetColumnsCount() + " and " + matrix2.GetStringsCount(), nameof(matrix1) + ", " + nameof(matrix2));
+            }
+
+            Matrix multiplication = new Matrix(matrix1.GetStringsCount(), matrix2.GetColumnsCount());
+
+            for (int i = 0; i < matrix1.GetStringsCount(); i++)
+            {
+                for (int j = 0; j < matrix2.GetColumnsCount(); j++)
+                {
+                    multiplication.strings[i].SetCoordinate(j, Vector.GetScalarMultiplication(matrix1.strings[i], matrix2.GetColumn(j)));
+                }
+            }
+
+            return multiplication;
         }
 
         public override string ToString()
@@ -125,12 +244,12 @@ namespace MatrixTask
             StringBuilder sb = new StringBuilder();
 
             sb.Append("{");
-            for (int i = 0; i < Vectors.Length - 1; i++)
+            for (int i = 0; i < strings.Length - 1; i++)
             {
-                sb.Append(Vectors[i].ToString());
+                sb.Append(strings[i]);
                 sb.Append(", ");
             }
-            sb.Append(Vectors[Vectors.Length - 1]);
+            sb.Append(strings[strings.Length - 1]);
             sb.Append("}");
 
             return sb.ToString();
--- CSharpCourseTasks/MatrixTask/Test.cs
+++ CSharpCourseTasks/MatrixTask/Test.cs
@@ -1,22 +1,39 @@
 ﻿using System;
+using VectorTask;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MatrixTask
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[,] pp = { { 1, 2, }, { 4, 5, }, { 6, 7 } };
+            double[,] strings = { { 1, 2 }, { 4, 5 }, { 6, 7 } };
 
-            Matrix matrix = new Matrix(pp);
-            matrix.GetTransposed();
-            Console.WriteLine(matrix);
+            Matrix matrix1 = new Matrix(strings);
+
+            matrix1.Transpose();
+
+            Console.WriteLine(matrix1);
+
+            Vector vector1 = new Vector(1.0, 5.0, 8.0);
+            Vector vector2 = new Vector(7.0, 9.0, 10.0, 3.0);
+
+            Matrix matrix2 = new Matrix(vector1, vector2);
+
+            Console.WriteLine(matrix2);
+
+            Matrix matrix3 = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+            matrix3.MultiplyOnVector(vector1);
+
+            Console.WriteLine(matrix3);
+
+            Matrix multiplier1 = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 } });
+            Matrix multiplier2 = new Matrix(new double[,] { { 7, 8, 9, 10 }, { 11, 12, 13, 14 }, { 15, 16, 17, 18 }});
+
+            Console.WriteLine(Matrix.GetMatrixMultiplication(multiplier1, multiplier2));
 
             Console.ReadKey();
         }
-    }
+}
 }
